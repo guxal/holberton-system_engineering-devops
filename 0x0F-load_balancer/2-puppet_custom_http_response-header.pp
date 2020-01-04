f433eb69783f76f7de039ac6da42f5d485348a85
guxal <jonathanacp93@gmail.com>
2020-01-04 22:32:14 +0000

    add task advance

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,46 @@
+# execute 'apt-get update'
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update', # command this resource will run
+}
+
+# install nginx package
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-update'],        # require 'apt-update' before installing
+}
+
+# ensure nginx service is running
+service { 'nginx':
+  ensure  => running,
+}
+
+# create directory if not exists
+exec { 'create-directory':
+  command => '/bin/mkdir -p /var/www/html/',
+  require => Package['nginx']           # require 'nginx' package before creating
+}
+
+# ensure index file exists
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Holberton School',        # index file
+  require => Exec['create-directory'],  # require directory for storage
+}
+
+# add redirect_me rewrite in site_available
+exec { 'redirect':
+  command => '/bin/sed -i \'s|_;|_;\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGlwu4 permanent;|g\' /etc/nginx/sites-available/default',
+  require => Package['nginx']
+}
+
+# add add_header rewrite in site_available
+exec { 'add-header':
+  command => '/bin/sed -i \'s|_;|_;\n\tadd_header \'X-Served-By\' \"\$HOSTNAME\"; |g\' /etc/nginx/sites-available/default',
+  require => Exec['redirect']
+}
+
+# restart nginx
+exec { 'restart':
+  command => '/bin/service nginx restart',
+  require => Exec['add-header']
+}
