26da1779c7f9e9cb3dae4ef68d5c3cfe2084c600
guxal <jonathanacp93@gmail.com>
2020-01-24 06:27:58 +0000

    add 3-live-in-fear

---
+++ 0x14-mysql/3-live-in-fear.sql
@@ -0,0 +1,6 @@
+-- This script live in fear
+CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'holberton';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+
