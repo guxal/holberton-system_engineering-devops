4b5d30196f3662a1c587b8164da8847ddaeceb59
guxal <jonathanacp93@gmail.com>
2020-01-24 06:09:04 +0000

    hotfix and add create database

--- 0x14-mysql/1-create-user_holberton.sql
+++ 0x14-mysql/1-create-user_holberton.sql
@@ -1,5 +1,6 @@
 -- This script create user holberton in database
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
 GRANT REPLICATION SLAVE ON *.* TO 'holberton_user'@'localhost';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
 SHOW GRANTS FOR 'holberton_user'@'localhost';
---
+++ 0x14-mysql/2-create_database.sql
@@ -0,0 +1,5 @@
+-- This script create database tyrell_corp
+create database IF NOT EXISTS tyrell_corp;
+use tyrell_corp;
+create table IF NOT EXISTS nexus6 (id int, name varchar(255));
+insert into nexus6(id, name) VALUES (1, 'Leon');
