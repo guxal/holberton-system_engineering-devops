ba8c9f2b114023d7cec2d6348df58a06c4881b24
guxal <jonathanacp93@gmail.com>
2020-01-24 06:27:47 +0000

    hotfix

--- 0x14-mysql/2-create_database.sql
+++ 0x14-mysql/2-create_database.sql
@@ -3,3 +3,4 @@ create database IF NOT EXISTS tyrell_corp;
 use tyrell_corp;
 create table IF NOT EXISTS nexus6 (id int, name varchar(255));
 insert into nexus6(id, name) VALUES (1, 'Leon');
+GRANT SELECT ON tyrell_corp.* TO 'holberton_user'@'localhost'
