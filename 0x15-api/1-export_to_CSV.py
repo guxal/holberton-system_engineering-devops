c0cc7524a1a30ee42ad2c7fcdad6f9f2cf5ce57b
guxal <jonathanacp93@gmail.com>
2020-02-05 05:18:40 +0000

    delete various lines

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
-    routes = ['users/{}', 'todos?userId={}', 'todos?userId={}&completed=true']
+    routes = ['users/{}', 'todos?userId={}']
 
     argv = sys.argv
 
@@ -16,17 +16,8 @@ if __name__ == "__main__":
         id = argv[1]
         user = requests.get(url + routes[0].format(id))
         allTask = requests.get(url + routes[1].format(id))
-        doneTask = requests.get(url + routes[2].format(id))
 
         name = user.json().get('name')
-        numberDoneTask = len(doneTask.json())
-        numberAllTask = len(allTask.json())
-
-        print("Employee {} is done with tasks({:d}/{:d}):"
-              .format(name, numberDoneTask, numberAllTask))
-
-        for done in doneTask.json():
-            print('\t ' + done.get('title'))
 
         with open(argv[1] + '.csv', 'w', newline='')as csvfile:
             csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
