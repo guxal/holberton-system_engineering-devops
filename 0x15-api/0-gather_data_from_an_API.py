82cbfd3a6ab30f945e82979bd1eba4734fd23903
guxal <jonathanacp93@gmail.com>
2020-02-05 04:05:24 +0000

    hotfix indent

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,17 +10,17 @@ if __name__ == "__main__":
 
     argv = sys.argv
 
-if len(argv) > 1:
-    user = requests.get(url + routes[0].format(argv[1]))
-    allTask = requests.get(url + routes[1].format(argv[1]))
-    doneTask = requests.get(url + routes[2].format(argv[1]))
+    if len(argv) > 1:
+        user = requests.get(url + routes[0].format(argv[1]))
+        allTask = requests.get(url + routes[1].format(argv[1]))
+        doneTask = requests.get(url + routes[2].format(argv[1]))
 
-    name = user.json().get('name')
-    numberDoneTask = len(doneTask.json())
-    numberAllTask = len(allTask.json())
+        name = user.json().get('name')
+        numberDoneTask = len(doneTask.json())
+        numberAllTask = len(allTask.json())
 
-    print("Employee {} is done with tasks({:d}/{:d}):"
-          .format(name, numberDoneTask, numberAllTask))
+        print("Employee {} is done with tasks({:d}/{:d}):"
+              .format(name, numberDoneTask, numberAllTask))
 
-    for done in doneTask.json():
-        print('\t ' + done.get('title'))
+        for done in doneTask.json():
+            print('\t ' + done.get('title'))
