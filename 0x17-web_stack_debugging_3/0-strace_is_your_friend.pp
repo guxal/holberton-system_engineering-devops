8dc18099c7b301e25e7306b16713fb738e607921
guxal <jonathanacp93@gmail.com>
2020-02-19 16:00:36 +0000

    hotfix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix the apache2 wordpress
 
 exec { 'sed':
-  command => '/usr/bin/sed -i \'s|phpp|php|g\' /var/www/html/wp-settings.php'
+  command => '/bin/sed -i \'s|phpp|php|g\' /var/www/html/wp-settings.php'
 }
